18aa64480a77571c4a4bed44891ad9ceb5c8b128
Fang0105 <nody0105@gmail.com>
2026-07-21 15:27:49 +0800

    fix: export count_parameters in models/__init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 """Fast-SCNN model package."""
 
-from models.fast_scnn import FastSCNN
+from models.fast_scnn import FastSCNN, count_parameters
 from models.fast_scnn_salient import FastSCNNSalient
 
-__all__ = ["FastSCNN", "FastSCNNSalient"]
+__all__ = ["FastSCNN", "FastSCNNSalient", "count_parameters"]
