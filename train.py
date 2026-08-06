89a42c383ce79b32bfbd7a46299a29bcfb126ff8
Fang0105 <nody0105@gmail.com>
2026-08-06 15:41:09 +0800

    fix: import torch.nn.functional as F in train.py

--- train.py
+++ train.py
@@ -35,6 +35,7 @@ from typing import Dict, List, Optional
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
